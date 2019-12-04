cd2e65b3acceebed42afcda3f358bdcc376232b1
Johan Thelin <e8johan@gmail.com>
2019-12-04 07:51:07 +0100

    Day 4

---
+++ 4/4b.py
@@ -0,0 +1,59 @@
+#/usr/bin/env python3
+
+# This my input
+startingValue = 128392 
+endingValue = 643281
+
+value = startingValue
+
+#while True:
+
+# Convert to individual numbers
+ns = [int(i) for i in str(value)]
+count = 0
+while True:
+    # The number finder loop - finds the next possible number
+    while True:
+        # Increase the number
+        pos = 1
+        while ns[-pos] == 9:
+            pos += 1
+        ns[-pos] += 1
+        while pos > 1:
+            pos -= 1
+            ns[-pos] = ns[-pos-1]
+
+        # Check for neighbours
+        pos = 0
+        found = False
+        while pos < 5:
+            if ns[pos] == ns[pos+1]:
+                # Ensure a unique pair, not a sequence
+                if not ((pos > 0 and ns[pos-1] == ns[pos]) or (pos < 4 and ns[pos+2] == ns[pos])):
+                    found = True
+                    break
+            pos += 1
+        
+        # Ensure increase
+        pos = 0
+        while pos < 5:
+            if ns[pos] > ns[pos+1]:
+                found = False
+                break
+            pos += 1
+
+        if found:
+            break
+
+    # Re-assemble the number
+    value = 0
+    for n in ns:
+        value = value*10 + n
+
+    # Check if we're done
+    if value < endingValue:
+        count += 1
+    else:
+        break
+
+print("Number of numbers: %s" % (count))
